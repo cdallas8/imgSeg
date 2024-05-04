35da66888495e035357f156ce98808b50d9e1948
cdallas8 <chiara.dallasanta@gmail.com>
2024-05-04 02:08:33 -0600

    update imgLoader

--- src/imgLoader.py
+++ src/imgLoader.py
@@ -1,19 +1,43 @@
-#############################################
+####################################################
 
 # Load modules
 
+import cellpose
+import numpy as np
 from skimage import io
+from cellpose import models
+from matplotlib import pyplot as plt
 
-#############################################
+####################################################
 
 # Declare path
 
 img_path = "data/images/C22_s2_x1_y0_Fluorescence_638_nm_Ex.tiff"
 
-#############################################
-
-img = io.imread(img_path)
+####################################################
+names = "test"
+# img = io.imread(img_path, as_gray=True, dtype = "uint8")
+img = cellpose.io.imread(img_path)
 
 print(img.shape)
+print(img.dtype)
+print(np.max(img))
+print(np.min(img))
 
-#############################################
+#plt.imshow(img, cmap = "gray")
+#plt.show()
+
+normal_img = (img - np.min(img))/(np.max(img) - np.min(img))
+
+print(normal_img.dtype)
+
+model = models.Cellpose(gpu=False, model_type='cyto')
+# alternative model type is 'nuclei'
+
+masks, flows, styles, diams = model.eval(normal_img, diameter=None, channels=[0,0])
+# channels = [0,0] gray scale image
+
+#print(masks)
+cellpose.io.save_masks(img, masks, flows, names, png = True)
+
+####################################################
