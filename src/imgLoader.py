297037460fc6bd8e1f59c6686526197243ce1205
cdallas8 <chiara.dallasanta@gmail.com>
2024-05-05 21:08:48 -0600

    update image loader

--- src/imgLoader.py
+++ src/imgLoader.py
@@ -3,6 +3,8 @@
 # Load modules
 
 import cellpose
+import polars
+import os
 import numpy as np
 from skimage import io
 from cellpose import models
@@ -15,14 +17,10 @@ from matplotlib import pyplot as plt
 img_path = "data/images/C22_s2_x1_y0_Fluorescence_638_nm_Ex.tiff"
 
 ####################################################
+
 names = "test"
 # img = io.imread(img_path, as_gray=True, dtype = "uint8")
 img = cellpose.io.imread(img_path)
-
-print(img.shape)
-print(img.dtype)
-print(np.max(img))
-print(np.min(img))
 
 #plt.imshow(img, cmap = "gray")
 #plt.show()
@@ -30,14 +28,30 @@ print(np.min(img))
 normal_img = (img - np.min(img))/(np.max(img) - np.min(img))
 
 print(normal_img.dtype)
+print(np.mean(normal_img))
+print(np.max(normal_img))
+
+#plt.hist(normal_img)
+#plt.show()
 
 model = models.Cellpose(gpu=False, model_type='cyto')
 # alternative model type is 'nuclei'
 
-masks, flows, styles, diams = model.eval(normal_img, diameter=None, channels=[0,0])
+chan = [0,0]
+masks, flows, styles, diams = model.eval(normal_img, diameter=None, channels=chan)
 # channels = [0,0] gray scale image
 
-#print(masks)
+plt.hist(masks)
+plt.show()
+
+print(masks.shape)
+print(flows[0].shape)
+print(masks[0].shape)
+
+fig = plt.figure()
+cellpose.plot.show_segmentation(fig, normal_img, masks, flows[0], channels = chan)
+plt.show()
+
 cellpose.io.save_masks(img, masks, flows, names, png = True)
 
 ####################################################
