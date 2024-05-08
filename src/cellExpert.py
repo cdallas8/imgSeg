6b1c526b4a2d2341e4886ed9a4cd78f0dd51c475
cdallas8 <chiara.dallasanta@gmail.com>
2024-05-07 23:21:34 -0600

    update main

--- src/cellExpert.py
+++ src/cellExpert.py
@@ -13,6 +13,7 @@ import os
 # import functions
 from util.cellSegmenter import *
 from util.cellMorph import *
+from util.imgStats import *
 
 ####################################################
 
@@ -21,14 +22,23 @@ from util.cellMorph import *
 path = inputDir
 files = os.listdir(path)
 
+####################################################
+
 counter = 0
 
+# initialize lists
 dfs = []
+stats = []
 
 for i in files:
     if counter < 5:
+        # calculate image stats
+        s = imgStats(os.path.join(path, i))
+        stats.append(s)
+        # segment image
         print("segmenting image: ", i)
         mask = cellSegmenter(os.path.join(path, i))
+        # cell morphology
         print("creating df with cell morphology info & coordinates")
         df = cellProps(os.path.join(path, i), mask, os.path.join(path, i))
         dfs.append(df)
@@ -36,7 +46,14 @@ for i in files:
     else:
         break
 
+# concatenate dataframes
+s = polars.concat(stats)
 df = polars.concat(dfs)
-print(df[1:10])
 
+saveHist(s)
+
+# write to csv
+s.write_csv(graphDir + "/" + "imgStats.csv")
 df.write_csv(graphDir + "/" + "CellMorph.csv")
+
+####################################################
