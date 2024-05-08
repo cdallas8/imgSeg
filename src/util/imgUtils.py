9343722a0e16ffcb73df70ab153d8924aa767b20
cdallas8 <chiara.dallasanta@gmail.com>
2024-05-07 23:12:29 -0600

    update image utils

--- src/util/imgUtils.py
+++ src/util/imgUtils.py
@@ -1,14 +1,21 @@
+####################################################
+
+# load modules
 import numpy as np
+import polars
 import matplotlib.pyplot as plt
 
+####################################################
 
 def normalize (i):
   return (i - np.min(i)) / (np.max(i) - np.min(i))
 
+####################################################
 
 def binarize (i, threshold = 0):
     return i > threshold
 
+####################################################
 
 def plots(img, img2, img3):
     plt.subplot(1, 3, 1)
@@ -17,3 +24,17 @@ def plots(img, img2, img3):
     plt.imshow(img2)
     plt.subplot(1, 3, 3)
     plt.imshow(img3)
+
+####################################################
+
+def stats(i):
+    m = np.mean(i)
+    s = np.std(i)
+    return m, s
+
+####################################################
+
+def statsToDf(f, m, s):
+    return polars.DataFrame({"file": f, "mean": m, "std": s})
+
+####################################################
