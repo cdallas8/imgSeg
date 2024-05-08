12b15b4bed643850156de60f153d0d28afba69ac
cdallas8 <chiara.dallasanta@gmail.com>
2024-05-07 21:56:02 -0600

    add config

---
+++ src/config/config.py
@@ -0,0 +1,19 @@
+####################################################
+
+import os
+
+####################################################
+
+projDir = os.getcwd()
+dataDir = projDir + '/' + 'data'
+inputDir = dataDir + '/' + 'images'
+outputDir = dataDir + '/' + 'output'
+binOutDir = outputDir + '/' + 'binary'
+maskOutDir = outputDir + '/' + 'mask'
+laOutDir = outputDir + '/' + 'label'
+srcDir = projDir + '/' + 'src'
+binDir = srcDir + '/' + 'bin'
+utilDir = srcDir + '/' + 'util'
+graphDir = srcDir + '/' + 'graph'
+
+####################################################
